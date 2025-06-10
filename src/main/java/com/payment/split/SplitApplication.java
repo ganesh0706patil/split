c9c9374e9980b90b6a9c426d6d15cbe405ace883
ganesh0706patil <ganeshpatil208507@gmail.com>
2025-06-11 00:20:48 +0530

    Initial commit

---
+++ src/main/java/com/payment/split/SplitApplication.java
@@ -0,0 +1,14 @@
+package com.payment.split;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
